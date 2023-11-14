b2ee40492a425283e049bbc68bbdee88fc421ad0
isaaccavallaro <isaaccavallaro@gmail.com>
2023-11-14 18:48:24 +1000

    New sections

--- src/components/Values.tsx
+++ src/components/Values.tsx
@@ -1,36 +1,80 @@
-// Values component
 import React from 'react';
 
 const Values: React.FC = () => {
   return (
-    <div id="values" className="section bg-gray-100 text-black p-8 relative font-brown-sugar">
-        <h2 className="text-3xl font-bold mb-6 mt-12 text-center relative z-10">
-        Our Values
-        </h2>
+    <section className="flex items-center bg-gray-100 lg:h-screen font-poppins dark:bg-gray-900">
+      <div className="justify-center max-w-6xl px-4 py-4 mx-auto lg:py-0">
+      <div className="px-4 pl-4 mb-6 border-l-4 border-blue-500">
+        <span className="text-sm text-gray-600 uppercase dark:text-gray-400">Why Pivot For Dancers?</span>
+        <h1 className="mt-2 text-3xl font-black text-gray-700 md:text-5xl dark:text-gray-300">
+          Our Values
+        </h1>
+      </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 relative z-10">
-        <div className="value flex flex-col items-center bg-gray-900 p-6 rounded-lg transform hover:scale-105 transition-transform h-full relative overflow-hidden">
-            <h3 className="text-xl font-semibold mb-2">Curiosity</h3>
-            <p className="leading-7 text-justify">
-            Pivot for Dancers encourages dancers to explore other interests simultaneously with pursuing a dance career. It doesn’t have to be one or the other.
-            </p>
-        </div>
+        <div className="grid grid-cols-1 gap-6 lg:grid-cols-3 md:grid-cols-2">
+          <div className="bg-white rounded-lg shadow-md dark:bg-gray-800">
+            <a href="#" className="">
+              <img
+                src="/assets/curiosity.jpg"
+                alt=""
+                className="object-cover w-full h-64 rounded-t-lg"
+              />
+            </a>
+            <div className="p-5">
+              <a href="#" className="">
+                <h2 className="mb-2 text-xl font-bold tracking-tight text-gray-700 dark:text-gray-400">
+                  Curiosity
+                </h2>
+              </a>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                Pivot for Dancers encourages dancers to explore other interests simultaneously with pursuing a dance career. It doesn’t have to be one or the other.
+              </p>
+            </div>
+          </div>
+          <div className="bg-white rounded-lg shadow-md dark:bg-gray-800">
+            <a href="#" className="">
+              <img
+                src="/assets/empowerment.jpg"
+                alt=""
+                className="object-cover w-full h-64 rounded-t-lg"
+              />
+            </a>
+            <div className="p-5">
+              <a href="#" className="">
+                <h2 className="mb-2 text-xl font-bold tracking-tight text-gray-700 dark:text-gray-400">
+                Empowerment
+                </h2>
+              </a>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+              Let’s ditch the idea that if we’re not dancing, we’re only merely surviving. Pivot is about finding fulfilling jobs for when times get tough or our passions change, instead of meaningless survival jobs that make us feel desperate.
+              </p>
 
-        <div className="value flex flex-col items-center bg-gray-900 p-6 rounded-lg transform hover:scale-105 transition-transform h-full relative overflow-hidden">
-            <h3 className="text-xl font-semibold mb-2">Empowerment</h3>
-            <p className="leading-7 text-justify">
-            Pivot for Dancers is not about survival jobs. It's about finding fulfilling jobs for tough times or when passions change, instead of meaningless survival jobs that make us feel desperate.
-            </p>
+            </div>
+          </div>
+          <div className="bg-white rounded-lg shadow-md dark:bg-gray-800">
+            <a href="#" className="">
+              <img
+                src="/assets/resilience.jpg"
+                alt=""
+                className="object-cover w-full h-64 rounded-t-lg"
+              />
+            </a>
+            <div className="p-5">
+              <a href="#" className="">
+                <h2 className="mb-2 text-xl font-bold tracking-tight text-gray-700 dark:text-gray-400">
+                  Resilience
+                </h2>
+              </a>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                You’ve made the courageous choice to see what else is out there for you. It’s that resilience you learned as an ambitious dancer that will help you through.
+              </p>
+              <div className="flex items-center justify-between">
+              </div>
+            </div>
+          </div>
         </div>
-
-        <div className="value flex flex-col items-center bg-gray-900 p-6 rounded-lg transform hover:scale-105 transition-transform h-full relative overflow-hidden">
-            <h3 className="text-xl font-semibold mb-2">Resilience</h3>
-            <p className="leading-7 text-justify">
-            Pivot for Dancers doesn't claim it'll be easy. We're all in this together. Everything you see here is a work in progress – and that’s a good thing! Enjoy the process.
-            </p>
-        </div>
-        </div>
-    </div>
+      </div>
+    </section>
   );
 };
 
