f2396e70d6858418bec2bf2d0e8b69bd6c75f4b4
isaaccavallaro <isaaccavallaro@gmail.com>
2023-11-21 08:25:13 +1000

    WIP: mobile sidebar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,8 +1,16 @@
 import React, { useState, useEffect } from 'react';
 
-function Navbar() {
+interface NavbarProps {
+  // Define any props if needed
+}
+
+const Navbar: React.FC<NavbarProps> = () => {
   const [activeSection, setActiveSection] = useState('');
   const [open, setOpen] = useState(false); // State to manage the mobile sidebar
+
+  const handleToggle = () => {
+    setOpen(!open);
+  };
 
   const scrollToSection = (id: string) => {
     const element = document.getElementById(id);
@@ -14,6 +22,7 @@ function Navbar() {
   const handleNavLinkClick = (id: string, event: React.MouseEvent<HTMLAnchorElement>) => {
     event.preventDefault();
     scrollToSection(id);
+    setOpen(false); // Close the mobile sidebar after clicking a link
   };
 
   useEffect(() => {
@@ -45,11 +54,11 @@ function Navbar() {
       <div className="max-w-6xl px-4 mx-auto">
         <nav className="fixed top-0 left-0 right-0 bg-green-800 dark:bg-gray-900 py-4 z-50">
           <div className="max-w-6xl mx-auto flex items-center justify-between">
-            <a href="/" className="text-xl font-medium leading-none text-gray-200 dark:text-gray-300">Logo</a>
+            <a href="/" className="lg:hidden text-xl font-medium leading-none text-gray-200 dark:text-gray-300">Logo</a>
             <div className="lg:hidden">
               <button
                 className="text-gray-200 dark:text-gray-300 focus:outline-none"
-                onClick={() => setOpen(!open)}
+                onClick={handleToggle}
               >
                 {/* Hamburger menu icon */}
                 <svg
@@ -62,7 +71,7 @@ function Navbar() {
                 </svg>
               </button>
             </div>
-            <ul className={`lg:w-auto lg:space-x-12 lg:items-center lg:flex ${open ? 'block' : 'hidden'}`}>
+            <ul className={`lg:w-auto lg:space-x-12 lg:items-center lg:flex ${open ? 'hidden' : 'block'}`}>
               <li><a href="#home" className={`text-sm text-gray-200 dark:text-gray-300 hover:text-blue-200 dark:hover:text-blue-300 ${activeSection === 'home' ? 'text-red-500' : ''}`} onClick={(e) => handleNavLinkClick('home', e)}>Home</a></li>
               <li><a href="#about" className={`text-sm text-gray-200 dark:text-gray-300 hover:text-blue-200 dark:hover:text-blue-300 ${activeSection === 'about' ? 'text-red-500' : ''}`} onClick={(e) => handleNavLinkClick('about', e)}>About</a></li>
               <li><a href="#products" className={`text-sm text-gray-200 dark:text-gray-300 hover:text-blue-200 dark:hover:text-blue-300 ${activeSection === 'products' ? 'text-red-500' : ''}`} onClick={(e) => handleNavLinkClick('products', e)}>Products</a></li>
@@ -71,28 +80,37 @@ function Navbar() {
           </div>
         </nav>
         {/* Mobile Sidebar */}
-        <div className={`fixed inset-0 w-full bg-gray-900 opacity-25 dark:bg-gray-400 lg:hidden ${open ? 'ease-in-opacity-100' : 'ease-out opacity-0'}`}>
-        </div>
-        <div className={`absolute inset-0 z-10 h-screen p-3 text-gray-700 duration-500 transform shadow-md dark:bg-gray-800 bg-blue-50 w-80 lg:hidden lg:transform-none lg:relative ${open ? 'ease-in-opacity-100 translate-x-0' : 'ease-out opacity-0 -translate-x-full'}`}>
-          <div className="flex justify-between px-5 py-2">
+        <div className={`lg:hidden fixed inset-0 z-20 bg-gray-900 bg-opacity-25 dark:bg-gray-400 ${open ? 'block' : 'hidden'}`} onClick={handleToggle}></div>
+        <div className={`lg:hidden fixed inset-y-0 left-0 z-30 w-64 bg-blue-50 dark:bg-gray-800 transform duration-300 ease-in-out ${open ? 'translate-x-0' : '-translate-x-full'}`}>
+          <div className="flex justify-between items-center px-5 py-2">
             <a className="text-2xl font-bold dark:text-gray-300" href="#">Logo</a>
-            <button className="rounded-md hover:text-blue-300 lg:hidden dark:text-gray-400" onClick={() => setOpen(false)}>
+            <button className="rounded-md hover:text-blue-300 dark:text-gray-400" onClick={handleToggle}>
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-x-circle" viewBox="0 0 16 16">
                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
                 <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
               </svg>
             </button>
           </div>
-          {/* Rest of the sidebar content */}
           <ul className="px-5 text-left mt-7">
-            {/* ... */}
+            <li className="pb-3">
+              <a href="" className="text-sm text-gray-700 hover:text-blue-400 dark:text-gray-100">Home</a>
+            </li>
+            <li className="pb-3">
+              <a href="" className="text-sm text-gray-700 hover:text-blue-400 dark:text-gray-400">About us</a>
+            </li>
+            <li className="pb-3">
+              <a href="" className="text-sm text-gray-700 hover:text-blue-400 dark:text-gray-400">Features</a>
+            </li>
+            <li className="pb-3">
+              <a href="" className="text-sm text-gray-700 hover:text-blue-400 dark:text-gray-400">Blog</a>
+            </li>
+            <li className="pb-3">
+              <a href="" className="text-sm text-gray-700 hover:text-blue-400 dark:text-gray-400">Testimonials</a>
+            </li>
           </ul>
-          <div className="border-t border-green-400 dark:border-gray-400 my-7"></div>
-          <div className="flex items-center px-5 lg:hidden">
-            {/* ... */}
-          </div>
+          {/* Rest of the sidebar content */}
         </div>
-        {/* ... */}
+        {/* ... Rest of your code */}
       </div>
     </section>
   );
