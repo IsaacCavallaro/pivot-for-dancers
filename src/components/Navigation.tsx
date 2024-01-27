d4b0cedaf9d311249bb5c67cb018bdc9853e47b4
isaaccavallaro <isaaccavallaro@gmail.com>
2024-01-27 13:49:56 +1000

    Highlights active section for navbar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -6,7 +6,7 @@ interface NavbarProps {
 
 const Navbar: React.FC<NavbarProps> = () => {
   const [activeSection, setActiveSection] = useState('');
-  const [open, setOpen] = useState(false); // State to manage the mobile sidebar
+  const [open, setOpen] = useState(false);
 
   const handleToggle = () => {
     setOpen(!open);
@@ -22,20 +22,22 @@ const Navbar: React.FC<NavbarProps> = () => {
   const handleNavLinkClick = (id: string, event: React.MouseEvent<HTMLAnchorElement>) => {
     event.preventDefault();
     scrollToSection(id);
-    setOpen(false); // Close the mobile sidebar after clicking a link
+    setOpen(false);
   };
 
   useEffect(() => {
     const handleScroll = () => {
       const sectionIds = ['home', 'about', 'products', 'contact'];
-
-      for (const id of sectionIds) {
+    
+      for (let i = sectionIds.length - 1; i >= 0; i--) {
+        const id = sectionIds[i];
         const section = document.getElementById(id);
+    
         if (section) {
           const rect = section.getBoundingClientRect();
-          const isVisible = rect.top >= 0 && rect.bottom <= window.innerHeight;
-
-          if (isVisible) {
+          const isMidpointVisible = rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2;
+    
+          if (isMidpointVisible) {
             setActiveSection(id);
             break;
           }
@@ -44,6 +46,7 @@ const Navbar: React.FC<NavbarProps> = () => {
     };
 
     window.addEventListener('scroll', handleScroll);
+
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
@@ -54,15 +57,14 @@ const Navbar: React.FC<NavbarProps> = () => {
       <div className="max-w-6xl px-4 mx-auto">
         <nav className="fixed top-0 left-0 right-0 bg-green-800 dark:bg-gray-900 py-4 z-50">
           <div className="max-w-6xl mx-auto flex items-center justify-between">
-          <a href="/" className="lg:block hidden">
-              <img src="/assets/logo.png" alt="Logo" className="h-8" /> {/* Add your image path and adjust the class for size */}
+            <a href="/" className="lg:block hidden">
+              <img src="/assets/logo.png" alt="Logo" className="h-8" />
             </a>
             <div className="lg:hidden">
               <button
                 className="text-gray-200 dark:text-gray-300 focus:outline-none"
                 onClick={handleToggle}
               >
-                {/* Hamburger menu icon */}
                 <svg
                   className="w-6 h-6 fill-current"
                   viewBox="0 0 24 24"
@@ -74,23 +76,42 @@ const Navbar: React.FC<NavbarProps> = () => {
               </button>
             </div>
             <ul className={`lg:w-auto lg:space-x-12 lg:items-center lg:flex ${!open ? 'hidden' : 'block'}`}>
-              {/* Conditionally render section links based on mobile sidebar open/close */}
               {!open && (
                 <>
-                  <li><a href="#home" className={`text-sm text-gray-200 dark:text-gray-300 hover:text-blue-200 dark:hover:text-blue-300 ${activeSection === 'home' ? 'text-red-500' : ''}`} onClick={(e) => handleNavLinkClick('home', e)}>Home</a></li>
-                  <li><a href="#about" className={`text-sm text-gray-200 dark:text-gray-300 hover:text-blue-200 dark:hover:text-blue-300 ${activeSection === 'about' ? 'text-red-500' : ''}`} onClick={(e) => handleNavLinkClick('about', e)}>About</a></li>
-                  <li><a href="#products" className={`text-sm text-gray-200 dark:text-gray-300 hover:text-blue-200 dark:hover:text-blue-300 ${activeSection === 'products' ? 'text-red-500' : ''}`} onClick={(e) => handleNavLinkClick('products', e)}>Products</a></li>
-                  <li><a href="#contact" className={`text-sm text-gray-200 dark:text-gray-300 hover:text-blue-200 dark:hover:text-blue-300 ${activeSection === 'contact' ? 'text-red-500' : ''}`} onClick={(e) => handleNavLinkClick('contact', e)}>Contact</a></li>
+                  <li>
+                  <a href="#home" className={`text-sm ${activeSection === 'home' ? 'tan-300' : 'text-gray-200 dark:text-gray-300'} hover:text-blue-200 dark:hover:text-blue-300`} onClick={(e) => handleNavLinkClick('home', e)}>
+                    Home
+                  </a>
+                  </li>
+                  <li>
+                    <a href="#about" className={`text-sm ${activeSection === 'about' ? 'tan-300' : 'text-gray-200 dark:text-gray-300'} hover:text-blue-200 dark:hover:text-blue-300`} onClick={(e) => handleNavLinkClick('about', e)}>
+                      About
+                    </a>
+                  </li>
+
+                  <li>
+                    <a href="#products" className={`text-sm ${activeSection === 'products' ? 'tan-300' : 'text-gray-200 dark:text-gray-300'} hover:text-blue-200 dark:hover:text-blue-300`} onClick={(e) => handleNavLinkClick('products', e)}>
+                      Products
+                    </a>
+                  </li>
+
+
+                  <li>
+                    <a href="#contact" className={`text-sm ${activeSection === 'contact' ? 'tan-300' : 'text-gray-200 dark:text-gray-300'} hover:text-blue-200 dark:hover:text-blue-300`} onClick={(e) => handleNavLinkClick('contact', e)}>
+                      Contact
+                    </a>
+                  </li>
                 </>
               )}
             </ul>
           </div>
         </nav>
-        {/* Mobile Sidebar */}
         <div className={`lg:hidden fixed inset-0 z-20 bg-gray-900 bg-opacity-25 dark:bg-gray-400 ${open ? 'block' : 'hidden'}`} onClick={handleToggle}></div>
         <div className={`lg:hidden fixed inset-y-0 left-0 z-30 w-64 bg-blue-50 dark:bg-gray-800 transform duration-300 ease-in-out ${open ? 'translate-x-0' : '-translate-x-full'}`}>
           <div className="flex justify-between items-center px-5 py-2">
-            <a className="text-2xl font-bold dark:text-gray-300" href="#">Logo</a>
+            <a className="text-2xl font-bold dark:text-gray-300" href="#">
+              Logo
+            </a>
             <button className="rounded-md hover:text-blue-300 dark:text-gray-400" onClick={handleToggle}>
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-x-circle" viewBox="0 0 16 16">
                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
@@ -99,26 +120,35 @@ const Navbar: React.FC<NavbarProps> = () => {
             </button>
           </div>
           <ul className="px-5 text-left mt-7">
-            {/* Sidebar links remain unchanged */}
             <li className="pb-3">
-              <a href="" className="text-sm text-gray-700 hover:text-blue-400 dark:text-gray-100">Home</a>
+              <a href="" className="text-sm text-gray-700 hover:text-blue-400 dark:text-gray-100">
+                Home
+              </a>
             </li>
             <li className="pb-3">
-              <a href="#about" className={`text-sm text-gray-200 dark:text-gray-300 hover:text-blue-200 dark:hover:text-blue-300 ${activeSection === 'about' ? 'text-red-500' : ''}`} onClick={(e) => handleNavLinkClick('about', e)}>About</a>
+                <a
+                  href="#about"
+                  className={`text-sm text-gray-200 dark:text-gray-300 hover:text-blue-200 dark:hover:text-blue-300 hover:tan-300 ${activeSection === 'about' ? 'tan-300' : ''}`}
+                  onClick={(e) => handleNavLinkClick('about', e)}
+                >
+                  About
+                </a>
+              </li>
+            <li className="pb-3">
+              <a href="#products" className={`text-sm text-gray-200 dark:text-gray-300 hover:text-blue-200 dark:hover:tan-300 ${activeSection === 'products' ? 'tan-300' : ''}`} onClick={(e) => handleNavLinkClick('products', e)}>
+                Products
+              </a>
             </li>
             <li className="pb-3">
-              <a href="#products" className={`text-sm text-gray-200 dark:text-gray-300 hover:text-blue-200 dark:hover:text-blue-300 ${activeSection === 'products' ? 'text-red-500' : ''}`} onClick={(e) => handleNavLinkClick('products', e)}>Products</a>
-            </li>
-            <li className="pb-3">
-              <a href="#contact" className={`text-sm text-gray-200 dark:text-gray-300 hover:text-blue-200 dark:hover:text-blue-300 ${activeSection === 'contact' ? 'text-red-500' : ''}`} onClick={(e) => handleNavLinkClick('contact', e)}>Contact</a>
+              <a href="#contact" className={`text-sm text-gray-200 dark:text-gray-300 hover:text-blue-200 dark:hover:tan-300 ${activeSection === 'contact' ? 'tan-300' : ''}`} onClick={(e) => handleNavLinkClick('contact', e)}>
+                Contact
+              </a>
             </li>
           </ul>
-          {/* Rest of the sidebar content */}
         </div>
-        {/* ... Rest of your code */}
       </div>
     </section>
   );
-}
+};
 
 export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
@@ -6,15 +5,21 @@ module.exports = {
   theme: {
     extend: {
       backgroundColor: {
-        'gray-900': '#433E49',
+        'gray-900': '#647C90',
         'purple-800': '#928490',
-        'tan-300': '#DBC1AD',
-        'pink-100': '#F3E8EB',
+        'tan-300': '#E2DED0',
+        'cool-gray-100': '#4E4F50',
+        'gray-700': '#746C70',
       },
       textColor: {
         'white': '#FFFFFF',
+        'gray-900': '#647C90',
+        'purple-800': '#928490',
+        'tan-300': '#E2DED0',
+        'cool-gray-100': '#4E4F50',
+        'gray-700': '#746C70',
       },
     },
   },
   plugins: [],
-}
+};
