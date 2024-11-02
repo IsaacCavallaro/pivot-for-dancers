1f1615a336c9a6b23f221f7b83ef9d163cbf09c4
isaaccavallaro <isaaccavallaro@gmail.com>
2024-11-02 10:12:56 +1000

    Reorder faqs

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -42,73 +42,82 @@ const FAQ = () => {
                                         </button>
                                     </div>
                                     <div className={`mt-1 text-sm text-white transition-all duration-1000 ease-in-out ${showAnswer1 ? 'opacity-100 max-h-96' : 'opacity-0 max-h-0 overflow-hidden'}`}>
-                                        Pivot for Dancers offers resources for professional dancers who are making a career change. We’re also a community of hundreds of dancers just like you and we hold events and workshops to help you connect with others who might be going through something similar to you. The best way to get involved is to join our email list where we share exclusive content and invitations.
+                                        Pivot for Dancers offers resources for professional dancers who are making a career change.
+                                        We're also a community of hundreds of dancers just like you and we hold events and workshops to
+                                        help you connect with others who might be going through something similar to you.
+                                        The best way to get involved is to join our email list where we share exclusive content and invitations.
+                                    </div>
+                                </div>
+                                {/* Who Runs Pivot */}
+                                <div className="flex flex-col justify-between w-full py-4 pl-4 pr-3 mb-3 bg-white rounded shadow dark:bg-gray-700">
+                                    <div className="flex items-center justify-between">
+                                        <span className="font-bold dark:text-white"> Who runs Pivot for Dancers? </span>
+                                        <button className="p-2 bg-purple-gray rounded" onClick={() => setShowAnswer2(!showAnswer2)}>
+                                            {showAnswer2 ? <MinusIcon /> : <PlusIcon />}
+                                        </button>
+                                    </div>
+                                    <div className={`mt-1 text-sm text-white transition-all duration-1000 ease-in-out ${showAnswer2 ? 'opacity-100 max-h-96' : 'opacity-0 max-h-0 overflow-hidden'}`}>
+                                        Pivot for Dancers is run by former professional dancers who have made their own career transitions.
+                                        Founded by Kaylee Randall in 2020 and supported by hundreds of dancers along the way,
+                                        our team is passionate about helping dancers thrive long after taking their final bow.
                                     </div>
                                 </div>
                                 {/* Panels */}
                                 <div className="flex flex-col justify-between w-full py-4 pl-4 pr-3 mb-3 bg-white rounded shadow dark:bg-gray-700">
                                     <div className="flex items-center justify-between">
                                         <span className="font-bold dark:text-white"> What are Pivot Panels? </span>
-                                        <button className="p-2 bg-purple-gray rounded" onClick={() => setShowAnswer2(!showAnswer2)}>
-                                            {showAnswer2 ? <MinusIcon /> : <PlusIcon />}
-                                        </button>
-                                    </div>
-                                    <div className={`mt-1 text-sm text-white transition-all duration-1000 ease-in-out ${showAnswer2 ? 'opacity-100 max-h-96' : 'opacity-0 max-h-0 overflow-hidden'}`}>
-                                        Pivot Panels are panel-style interviews with professional dancers who have successfully changed careers. These valuable resources are available to help inspire you to take a leap of faith and may give you some ideas of how you may want to pivot your career.
-                                    </div>
-                                </div>
-                                {/* Runs Pivot */}
-                                <div className="flex flex-col justify-between w-full py-4 pl-4 pr-3 mb-3 bg-white rounded shadow dark:bg-gray-700">
-                                    <div className="flex items-center justify-between">
-                                        <span className="font-bold dark:text-white"> Who runs Pivot for Dancers? </span>
                                         <button className="p-2 bg-purple-gray rounded" onClick={() => setShowAnswer3(!showAnswer3)}>
                                             {showAnswer3 ? <MinusIcon /> : <PlusIcon />}
                                         </button>
                                     </div>
                                     <div className={`mt-1 text-sm text-white transition-all duration-1000 ease-in-out ${showAnswer3 ? 'opacity-100 max-h-96' : 'opacity-0 max-h-0 overflow-hidden'}`}>
-                                        Pivot for Dancers is run by former professional dancers who have made their own career transitions. Founded by Kaylee Randall in 2020 and supported by hundreds of dancers along the way, our team is passionate about helping dancers thrive long after taking their final bow.
+                                        Pivot Panels are panel-style interviews with professional dancers who have successfully changed careers.
+                                        These valuable resources are available to help inspire you to take a leap of faith and
+                                        may give you some ideas of how you may want to pivot your career.
+                                    </div>
+                                </div>
+                                {/* Ebook */}
+                                <div className="flex flex-col justify-between w-full py-4 pl-4 pr-3 mb-3 bg-white rounded shadow dark:bg-gray-700">
+                                    <div className="flex items-center justify-between">
+                                        <span className="font-bold dark:text-white"> What is the 'How to Pivot' ebook?</span>
+                                        <button className="p-2 bg-purple-gray rounded" onClick={() => setShowAnswer4(!showAnswer4)}>
+                                            {showAnswer4 ? <MinusIcon /> : <PlusIcon />}
+                                        </button>
+                                    </div>
+                                    <div className={`mt-1 text-sm text-white transition-all duration-1000 ease-in-out ${showAnswer4 ? 'opacity-100 max-h-96' : 'opacity-0 max-h-0 overflow-hidden'}`}>
+                                        Written by our founder, Kaylee Randall, 'How to Pivot' is an actionable, dancer-specific guide to help
+                                        you find meaningful work of the stage.In 10 chapters, Kaylee dives deep into psychological patterns that might
+                                        be holding you back and step-by-step activities to help you actually move on to your next stage.
+                                        With real-life examples from Kaylee's own career transition, 'How to Pivot' is essential reading for any dancer struggling with their next steps.
+                                    </div>
+                                </div>
+                                {/* Course */}
+                                <div className="flex flex-col justify-between w-full py-4 pl-4 pr-3 mb-3 bg-white rounded shadow dark:bg-gray-700">
+                                    <div className="flex items-center justify-between">
+                                        <span className="font-bold dark:text-white"> What is the 'Happy Trails' Mini Course?</span>
+                                        <button className="p-2 bg-purple-gray rounded" onClick={() => setShowAnswer5(!showAnswer5)}>
+                                            {showAnswer5 ? <MinusIcon /> : <PlusIcon />}
+                                        </button>
+                                    </div>
+                                    <div className={`mt-1 text-sm text-white transition-all duration-1000 ease-in-out ${showAnswer5 ? 'opacity-100 max-h-96' : 'opacity-0 max-h-0 overflow-hidden'}`}>
+                                        If you're a professional dancer, you probably don't have a ton of time.
+                                        That's why we developed a quick but effective mini course to help you plan your pivot with an in-depth, career change roadmap.
+                                        We lay it all out for you and take the guesswork out of changing careers. Our 5-year career roadmap can help you stay on
+                                        track and finally feel confident about your next steps.
                                     </div>
                                 </div>
                                 {/* Mentorship */}
                                 <div className="flex flex-col justify-between w-full py-4 pl-4 pr-3 mb-3 bg-white rounded shadow dark:bg-gray-700">
                                     <div className="flex items-center justify-between">
                                         <span className="font-bold dark:text-white"> What is the Pivot for Dancers Mentorship Program?</span>
-                                        <button className="p-2 bg-purple-gray rounded" onClick={() => setShowAnswer4(!showAnswer4)}>
-                                            {showAnswer4 ? <MinusIcon /> : <PlusIcon />}
-                                        </button>
-                                    </div>
-                                    <div className={`mt-1 text-sm text-white transition-all duration-1000 ease-in-out ${showAnswer4 ? 'opacity-100 max-h-96' : 'opacity-0 max-h-0 overflow-hidden'}`}>
-                                        The Pivot for Dancers Mentorship Program is private, one-on-one support for professional dancers to help you find meaningful work off the stage.
-                                        Structured with clear goal-setting and targeted reflection, our Mentorship Program is designed to offer actionable results.
-                                        You’ll be paired with an experienced, former professional dancer to guide you through these challenging times.
-                                    </div>
-                                </div>
-                                {/* Ebook */}
-                                <div className="flex flex-col justify-between w-full py-4 pl-4 pr-3 mb-3 bg-white rounded shadow dark:bg-gray-700">
-                                    <div className="flex items-center justify-between">
-                                        <span className="font-bold dark:text-white"> What is the ‘How to Pivot’ ebook?</span>
-                                        <button className="p-2 bg-purple-gray rounded" onClick={() => setShowAnswer5(!showAnswer5)}>
-                                            {showAnswer5 ? <MinusIcon /> : <PlusIcon />}
-                                        </button>
-                                    </div>
-                                    <div className={`mt-1 text-sm text-white transition-all duration-1000 ease-in-out ${showAnswer5 ? 'opacity-100 max-h-96' : 'opacity-0 max-h-0 overflow-hidden'}`}>
-                                        Written by our founder, Kaylee Randall, ‘How to Pivot’ is an actionable, dancer-specific guide to help you find meaningful work of the stage.
-                                        In 10 chapters, Kaylee dives deep into psychological patterns that might be holding you back and step-by-step activities to help you actually move on to your next stage.
-                                        With real-life examples from Kaylee’s own career transition, ‘How to Pivot’ is essential reading for any dancer struggling with their next steps.
-                                    </div>
-                                </div>
-                                {/* Course */}
-                                <div className="flex flex-col justify-between w-full py-4 pl-4 pr-3 mb-3 bg-white rounded shadow dark:bg-gray-700">
-                                    <div className="flex items-center justify-between">
-                                        <span className="font-bold dark:text-white"> What is the ‘Happy Trails’ Mini Course?</span>
                                         <button className="p-2 bg-purple-gray rounded" onClick={() => setShowAnswer6(!showAnswer6)}>
                                             {showAnswer6 ? <MinusIcon /> : <PlusIcon />}
                                         </button>
                                     </div>
                                     <div className={`mt-1 text-sm text-white transition-all duration-1000 ease-in-out ${showAnswer6 ? 'opacity-100 max-h-96' : 'opacity-0 max-h-0 overflow-hidden'}`}>
-                                        If you’re a professional dancer, you probably don’t have a ton of time.
-                                        That’s why we developed a quick but effective mini course to help you plan your pivot with an in-depth, career change roadmap.
-                                        We lay it all out for you and take the guesswork out of changing careers. Our 5-year career roadmap can help you stay on track and finally feel confident about your next steps.
+                                        The Pivot for Dancers Mentorship Program is private, one-on-one support for professional dancers to help you find meaningful work off the stage.
+                                        Structured with clear goal-setting and targeted reflection, our Mentorship Program is designed to offer actionable results.
+                                        You'll be paired with an experienced, former professional dancer to guide you through these challenging times.
                                     </div>
                                 </div>
                             </div>
