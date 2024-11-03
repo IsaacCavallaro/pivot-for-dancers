d4eb49fbd193afc8f72a00c5db7569fb4e61e71d
isaaccavallaro <isaaccavallaro@gmail.com>
2024-11-03 17:23:32 +1000

    Fix navbar mobile and tablet sliding

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -27,8 +27,8 @@ const Navbar: React.FC<NavbarProps> = () => {
 
   useEffect(() => {
     const handleScroll = () => {
-      const sectionIds = ['home', 'about', 'products', 'contact'];
-      let threshold = 0.8; // Adjust this threshold as needed
+      const sectionIds = ['home', 'about', 'products'];
+      let threshold = 0.8;
 
       for (let i = sectionIds.length - 1; i >= 0; i--) {
         const id = sectionIds[i];
@@ -37,15 +37,9 @@ const Navbar: React.FC<NavbarProps> = () => {
         if (section) {
           const rect = section.getBoundingClientRect();
           const isMidpointVisible = rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2;
-
-          if (isMidpointVisible) {
-            setActiveSection(id);
-            break;
-          }
-
           const isInThreshold = rect.top <= window.innerHeight * threshold && rect.bottom >= window.innerHeight * threshold;
 
-          if (isInThreshold) {
+          if (isMidpointVisible || isInThreshold) {
             setActiveSection(id);
             break;
           }
@@ -54,25 +48,49 @@ const Navbar: React.FC<NavbarProps> = () => {
     };
 
     window.addEventListener('scroll', handleScroll);
-
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
 
-  const joinUsClassName = `text-sm px-4 py-2 rounded-full bg-purple-gray text-white opacity-80 hover:opacity-100 hover:bg-purple-gray`;
+  const joinUsClassName = 'text-sm px-4 py-2 rounded-full bg-purple-gray text-white opacity-80 hover:opacity-100 hover:bg-purple-gray';
 
   return (
     <section className="bg-dark-gray max-device-width">
       <div className="max-w-6xl px-4 mx-auto">
         <nav className="fixed top-0 left-0 right-0 bg-gray-100 dark:bg-gray-800 py-4 z-50">
           <div className="max-w-6xl mx-auto flex items-center justify-between">
-            {/* Logo for both Desktop and Mobile */}
-            <a href="/" className="block pl-4 md:pl-6"> {/* Added padding here */}
+            <a href="/" className="block pl-4 md:pl-6">
               <img src="/assets/logo.png" alt="Logo" className="h-8" />
             </a>
-            <div className="lg:hidden flex items-center justify-end w-full pr-4 md:pr-6"> {/* Added padding here */}
-              {/* Hamburger Button */}
+
+            {/* Desktop Navigation Links */}
+            <ul className="hidden lg:flex lg:space-x-8 lg:items-center">
+              {['home', 'products', 'about'].map((section) => (
+                <li key={section}>
+                  <a
+                    href={`#${section}`}
+                    className={`text-sm ${activeSection === section ? 'tan-300' : 'text-gray-200 dark:text-gray-300'
+                      } hover:text-light-gray`}
+                    onClick={(e) => handleNavLinkClick(section, e)}
+                  >
+                    {section.toUpperCase()}
+                  </a>
+                </li>
+              ))}
+              <li>
+                <a
+                  href="#home"
+                  className={joinUsClassName}
+                  onClick={(e) => handleNavLinkClick('home', e)}
+                >
+                  JOIN US
+                </a>
+              </li>
+            </ul>
+
+            {/* Mobile Hamburger Button */}
+            <div className="lg:hidden flex items-center justify-end w-full pr-4 md:pr-6">
               <button
                 className="text-gray-200 dark:text-gray-300 focus:outline-none"
                 onClick={handleToggle}
@@ -87,81 +105,40 @@ const Navbar: React.FC<NavbarProps> = () => {
                 </svg>
               </button>
             </div>
-            <ul className={`lg:w-auto lg:space-x-12 lg:items-center lg:flex ${!open ? 'hidden' : 'block'}`}>
-              {!open && (
-                <>
-                  <li>
-                    <a href="#home" className={`text-sm ${activeSection === 'home' ? 'tan-300' : 'text-gray-200 dark:text-gray-300'} hover:text-light-gray`} onClick={(e) => handleNavLinkClick('home', e)}>
-                      HOME
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#products" className={`text-sm ${activeSection === 'products' ? 'tan-300' : 'text-gray-200 dark:text-gray-300'} hover:text-light-gray`} onClick={(e) => handleNavLinkClick('products', e)}>
-                      PRODUCTS
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#about" className={`text-sm ${activeSection === 'about' ? 'tan-300' : 'text-gray-200 dark:text-gray-300'} hover:text-light-gray`} onClick={(e) => handleNavLinkClick('about', e)}>
-                      ABOUT
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="#contact"
-                      className={joinUsClassName}
-                      onClick={(e) => handleNavLinkClick('contact', e)}
-                    >
-                      JOIN US
-                    </a>
-                  </li>
-                </>
-              )}
-            </ul>
-          </div>
-        </nav>
-        <div className={`lg:hidden fixed inset-0 z-20 bg-gray-900 bg-opacity-25 dark:bg-gray-400 ${open ? 'block' : 'hidden'}`} onClick={handleToggle}></div>
-        <div className={`lg:hidden fixed inset-y-0 right-0 z-30 w-64 bg-blue-50 dark:bg-gray-800 transform duration-300 ease-in-out ${open ? 'translate-x-0' : 'translate-x-full'}`}>
-          <div className="flex justify-between items-center px-5 py-2">
-            {/* Mobile Sidebar Logo */}
-            <a href="/" className="ml-auto">
-              <img src="/assets/logo.png" alt="Logo" className="h-8" />
-            </a>
-            {/* Close Button */}
-            <button className="rounded-md hover:text-blue-300 dark:text-gray-400" onClick={handleToggle}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-x-circle" viewBox="0 0 16 16">
-                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
-              </svg>
-            </button>
           </div>
 
-          <ul className="px-5 text-left mt-7">
-            <li className="pb-3">
-              <a href="/" className="text-sm text-gray-700 hover:text-blue-400 dark:text-gray-100">
-                HOME
-              </a>
-            </li>
-            <li className="pb-3">
-              <a
-                href="#about"
-                className={`text-sm text-gray-200 dark:text-gray-300 hover:text-blue-200 dark:hover:text-blue-300 hover:tan-300 ${activeSection === 'about' ? 'tan-300' : ''}`}
-                onClick={(e) => handleNavLinkClick('about', e)}
-              >
-                ABOUT
-              </a>
-            </li>
-            <li className="pb-3">
-              <a href="#products" className={`text-sm text-gray-200 dark:text-gray-300 hover:text-blue-200 dark:hover:tan-300 ${activeSection === 'products' ? 'tan-300' : ''}`} onClick={(e) => handleNavLinkClick('products', e)}>
-                PRODUCTS
-              </a>
-            </li>
-            <li className="pb-3">
-              <a href="#contact" className={joinUsClassName} onClick={(e) => handleNavLinkClick('contact', e)}>
-                JOIN US
-              </a>
-            </li>
-          </ul>
-        </div>
+          {/* Mobile Sidebar Menu */}
+          <div
+            className={`lg:hidden fixed inset-0 z-40 bg-gray-900 bg-opacity-90 transition-transform duration-300 ease-in-out ${open ? 'transform translate-x-0' : 'transform translate-x-full'
+              }`}
+          >
+            <div className="flex flex-col justify-center items-center h-full">
+              <ul className="space-y-6 text-center">
+                {['home', 'products', 'about'].map((section) => (
+                  <li key={section}>
+                    <a
+                      href={`#${section}`}
+                      className={`text-2xl ${activeSection === section ? 'tan-300' : 'text-gray-200 dark:text-gray-300'
+                        } hover:text-light-gray`}
+                      onClick={(e) => handleNavLinkClick(section, e)}
+                    >
+                      {section.toUpperCase()}
+                    </a>
+                  </li>
+                ))}
+                <li>
+                  <a
+                    href="#home"
+                    className={joinUsClassName}
+                    onClick={(e) => handleNavLinkClick('home', e)}
+                  >
+                    JOIN US
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
       </div>
     </section>
   );
