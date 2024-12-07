75ff3004510c806926adef79cd9fce6e8e986c02
isaaccavallaro <isaaccavallaro@gmail.com>
2024-12-07 15:00:34 +1000

    More footer layout updates

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Footer: React.FC = () => {
   const iconSize = 'h-8 w-8'; // for social media
   const socialMediaIconClass = `text-white p-2 rounded-full flex items-center justify-center ${iconSize} transition-transform transform hover:scale-110`;
-  const signUpUrl = 'https://mailchi.mp/2129c6018f7d/pivot-for-dancers-email-sign-up';
-  const joinUsClassName = 'px-6 py-3 text-sm rounded-md bg-purple-gray text-white opacity-80 hover:opacity-100 hover:bg-purple-gray';
+  const buttonClass = `
+    px-6 py-4 
+    text-sm font-semibold 
+    text-gray-100 
+    bg-purple-gray 
+    rounded-md 
+    hover:bg-purple-gray 
+    opacity-80 hover:opacity-100 
+    w-full md:w-auto
+  `.trim();
+  const footerInputClass = `
+    px-4 py-4 
+    text-sm text-gray-900 
+    placeholder-gray-400 
+    bg-gray-100 
+    border border-gray-300 
+    rounded-md 
+    dark:text-gray-400 
+    dark:placeholder-gray-400 
+    dark:bg-gray-700 
+    dark:border-gray-700 
+    w-full md:w-auto
+    w-full md:w-96
+  `.trim();
+
+  const [email, setEmail] = useState('');
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    window.location.href = `https://pivotfordancers.us19.list-manage.com/subscribe/post?u=be8fecbf3f1babc7628da411c&amp;id=e5d51bd2a0&amp;f_id=00c396e4f0&EMAIL=${encodeURIComponent(
+      email
+    )}`;
+  };
 
   return (
     <footer className="bg-gray-800 text-white">
       {/* Top Section */}
-      <div className="max-w-6xl mx-auto px-6 py-12 flex flex-col items-center space-y-8 md:flex-row md:space-y-0 md:space-x-6 md:justify-center md:items-center">
+      <div className="max-w-6xl mx-auto px-6 py-4 flex flex-col items-center md:flex-row md:justify-between">
         {/* Social Media Links */}
-        <div className="flex space-x-4">
+        <div className="flex flex-row space-x-4 items-center pb-4 sm:pb-0">
           <a
             href="https://www.facebook.com/pivotfordancers/"
             className={`${socialMediaIconClass} bg-blue-500 hover:bg-blue-400`}
@@ -50,18 +85,26 @@ const Footer: React.FC = () => {
           </a>
         </div>
 
-        {/* Join Us Button */}
-        <a
-          href={signUpUrl}
-          className={joinUsClassName}
-          target="_blank"
-          rel="noopener noreferrer"
+        {/* Join Us Form */}
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col md:flex-row items-center md:space-x-4 space-y-4 md:space-y-0 w-full md:w-auto"
         >
-          JOIN US
-        </a>
+          <input
+            type="email"
+            placeholder="Enter your email"
+            value={email}
+            onChange={handleEmailChange}
+            className={`${footerInputClass}`}
+          />
+
+          <button type="submit" className={`${buttonClass} max-w-md`}>
+            JOIN US
+          </button>
+        </form>
       </div>
 
-      {/* Border divder */}
+      {/* Border Divider */}
       <div className="border-t border-gray-600 mx-6"></div>
 
       {/* Bottom Section */}
