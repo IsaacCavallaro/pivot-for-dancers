f80098a2c425e1bd2ef89a1c5a4173fe48836f30
isaaccavallaro <isaaccavallaro@gmail.com>
2025-10-09 10:28:20 +1000

    add files inn prep for merge

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    basePath: '/pivot-for-dancers',
+    trailingSlash: true,
+};
+
+module.exports = nextConfig;
