9ff2a8b60ea6de5fff0530b264c06e83a1e7c364
isaaccavallaro <isaaccavallaro@gmail.com>
2025-10-09 12:27:29 +1000

    more config updates after deploy

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,16 +7,6 @@ export default function Document() {
                 {/* Favicon */}
                 <link rel="icon" href="/assets/logo.png" />
                 <link rel="apple-touch-icon" href="/assets/logo.png" />
-
-                {/* Open Graph / Social Media Meta Tags */}
-                <meta property="og:image" content="/assets/logo.png" />
-                <meta property="og:image:width" content="1200" />
-                <meta property="og:image:height" content="630" />
-                <meta property="og:type" content="website" />
-
-                {/* Twitter Card Meta Tags */}
-                <meta name="twitter:card" content="summary_large_image" />
-                <meta name="twitter:image" content="/assets/logo.png" />
             </Head>
             <body>
                 <Main />
